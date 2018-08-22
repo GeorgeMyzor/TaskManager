4bf2a6ce0c1e5e945be6aac20a0f6ea5b7029b2c
Yahor_Rozum@epam.com <Yahor_Rozum@epam.com>
2018-08-22 16:59:21 +0300

    1-9 task

---
+++ src/app/tasks/index.ts
@@ -0,0 +1,2 @@
+export * from './components/task/task.component';
+export * from './components/task-list/task-list.component';
---
+++ src/app/tasks/tasks.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+import { TasksRoutingModule } from './tasks-routing.module';
+import { TaskListComponent, TaskComponent } from '.'; 
+
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    TasksRoutingModule
+  ],
+  declarations: [TaskListComponent, TaskComponent]
+})
+export class TasksModule { }
